f23284aa4b6bda67b3a5ed748e40935e6bfa7673
williamrolando88 <william.rolando88@hotmail.com>
2021-11-22 15:26:01 -0500

    Add local storage extraction function and remove button functionality

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
-let books = [];
+window.onload = () => {
+  let books = [];
 
-const addBooksButton = document.querySelector('#add-book');
-const bookTitle = document.querySelector('#book-title');
-const bookAuthor = document.querySelector('#book-author');
+  const addBooksButton = document.querySelector('#add-book');
+  const bookTitle = document.querySelector('#book-title');
+  const bookAuthor = document.querySelector('#book-author');
 
-
-const addBook = (title, author) => {
-  let newBook = {
-    title: title,
-    author: author,
+  const addBook = (title, author) => {
+    let newBook = {
+      title: title,
+      author: author,
+    };
+    books.push(newBook);
+    store(books);
   };
-  books.push(newBook);
-  store(books);
-};
 
-const displayBooks = () => {
-  let booksLocalStorage = JSON.parse(localStorage.getItem('books'));
-  let html = '';
-  booksLocalStorage.forEach(book => {
-    html += `<div class="book">
+  const displayBooks = () => {
+    let booksLocalStorage = JSON.parse(localStorage.getItem('books'));
+    books = booksLocalStorage;
+    let html = '';
+    booksLocalStorage.forEach((book, index) => {
+      html += `<div class="book">
                 <h4>${book.title}</h4>
                 <p>${book.author}</p>
-                <button class="removeBtn">Remove</button>
+                <button class="removeBtn" id="${index}">Remove</button>
                 <hr />
               </div>`;
+    });
+    document.querySelector('#books-container').innerHTML = html;
+
+    const removeBook = document.querySelectorAll('.removeBtn');
+
+    [...removeBook].forEach((button) => {
+      button.addEventListener('click', (e) => {
+        books.splice(e.target.id, 1);
+        store(books);
+        displayBooks();
+      });
+    });
+  };
+
+  addBooksButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    addBook(bookTitle.value, bookAuthor.value);
+    displayBooks();
   });
-  document.querySelector('#books-container').innerHTML = html;
-}
 
-addBooksButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  addBook(bookTitle.value, bookAuthor.value);
-  displayBooks()
-});
+  const store = (books) => {
+    localStorage.setItem('books', JSON.stringify(books));
+  };
 
-const store = (books) => {
-  localStorage.setItem('books', JSON.stringify(books));
-}
+  displayBooks();
+};
