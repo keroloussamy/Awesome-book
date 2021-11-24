0924f29ab8c21781cf1a8bf3b97eb47b5e023a95
keroloussamy <keroloussamy98@gmail.com>
2021-11-24 19:27:01 +0200

    Add style to the form, Fix linters

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
-     '*.html',
-     '*.js',
+    '*.html',
+    '*.js',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -11,4 +11,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
